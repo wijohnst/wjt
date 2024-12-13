ebbfbcb8a968c33889285dfa5ce43ac3600e5cde
Will Johnston <wijohnst@gmail.com>
2024-12-13 17:28:23 -0500

    feat: uses package version (#25)

--- apps/wjt/src/controllers/v1/home.ts
+++ apps/wjt/src/controllers/v1/home.ts
@@ -1,5 +1,8 @@
 import { Context } from 'koa';
 
+//@ts-expect-error - package not found
+import pkg from '#package';
+
 export const home = async (ctx: Context) => {
-  await ctx.render('home.pug', { version: 'v0.0.0' });
+  await ctx.render('home.pug', { version: pkg.version });
 };
