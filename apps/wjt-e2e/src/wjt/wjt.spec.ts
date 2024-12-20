e05908f26867a9bd1724ac2e41022b7c9dcf0e2c
wijohnst <wijohnst@gmail.com>
2024-12-20 11:16:24 -0800

    chore: skips install on local

--- apps/wjt-e2e/src/wjt/wjt.spec.ts
+++ apps/wjt-e2e/src/wjt/wjt.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('/');
-  const version = page.locator('text=version: 0.5.0');
+  const version = page.locator('text=version: ');
 
   await expect(version).toBeVisible();
 });
