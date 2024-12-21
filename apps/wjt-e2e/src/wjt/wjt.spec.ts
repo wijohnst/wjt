9b72788546c0c8c4e526aa201c545aac36d56a6f
wijohnst <wijohnst@gmail.com>
2024-12-21 15:23:47 -0800

    feat: implements basic static blog rendering

--- apps/wjt-e2e/src/wjt/wjt.spec.ts
+++ apps/wjt-e2e/src/wjt/wjt.spec.ts
@@ -4,12 +4,12 @@ test('version number should be displayed', async ({ page }) => {
   await page.goto('/');
   const version = page.locator('text=version: ');
 
-  await expect(version).toBeVisible();
+  expect(version).toBeVisible();
 });
 
 test('heading should be visible', async ({ page }) => {
   await page.goto('/');
   const mainHeading = page.getByText('willjohnston.tech');
 
-  await expect(mainHeading).toBeVisible();
+  expect(mainHeading).toBeVisible();
 });
