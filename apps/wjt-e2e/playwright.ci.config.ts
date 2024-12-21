838ba47f33b7a5a02492015df08bd7a120acfb07
wijohnst <wijohnst@gmail.com>
2024-12-21 09:34:25 -0800

    chore: removes latest tag from run cmd

--- apps/wjt-e2e/playwright.ci.config.ts
+++ apps/wjt-e2e/playwright.ci.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   webServer: {
     command:
-      'docker run -p 4200:4200 --name wjt-e2e-latest --rm -d wijohnst/wjt:latest',
+      'docker run -p 4200:4200 --name wjt-e2e-latest --rm -d wijohnst/wjt',
     reuseExistingServer: false,
   },
   projects: [
