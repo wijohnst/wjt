471cb4a9e4869c14f37c6b99d44c6ea2b54fe7a3
wijohnst <wijohnst@gmail.com>
2024-12-20 19:41:11 -0800

    chore: adds no_proxy env var

--- apps/wjt-e2e/src/wjt/wjt.spec.ts
+++ apps/wjt-e2e/src/wjt/wjt.spec.ts
@@ -1,8 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('version number should be displayed', async ({ page }) => {
   await page.goto('/');
   const version = page.locator('text=version: ');
 
   await expect(version).toBeVisible();
+});
+
+test('heading should be visible', async ({ page }) => {
+  await page.goto('/');
+  const mainHeading = page.getByText('willjohnston.tech');
+
+  await expect(mainHeading).toBeVisible();
 });
