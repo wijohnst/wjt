8e6a78f3aac895fe92bdff1c1a96203b54d43b7e
wijohnst <wijohnst@gmail.com>
2025-03-16 14:25:25 -0700

    chore: implements update method

--- libs/markdown/src/lib/Markdown/Markdown.spec.ts
+++ libs/markdown/src/lib/Markdown/Markdown.spec.ts
@@ -1,6 +1,7 @@
 import { Markdown } from './Markdown';
 import { Doc as Document } from '../Doc';
 import mock from 'mock-fs';
+import fs from 'fs';
 
 describe('Markdown Document', () => {
   test('should be defined', () => {
@@ -43,6 +44,48 @@ describe('Markdown Document', () => {
       const expectedValue = Buffer.from('file content');
 
       expect(sut.value).toEqual(expectedValue);
+    });
+  });
+
+  describe('update', () => {
+    let sut: Markdown;
+
+    const mockFileSystem = {
+      'path/to/file.md': 'update me',
+    };
+
+    beforeEach(() => {
+      mock(mockFileSystem);
+      sut = new Markdown('path/to/file.md');
+    });
+
+    afterEach(() => {
+      mock.restore();
+    });
+
+    test('should be defined', () => {
+      expect(sut.update).toBeDefined();
+    });
+
+    test('should update the document', async () => {
+      const matcher = new RegExp('update me');
+      const replacement = 'updated';
+
+      await sut.update({ matcher, replacement });
+
+      const expectedValue = Buffer.from('updated');
+      const value = fs.readFileSync('path/to/file.md');
+
+      expect(value).toEqual(expectedValue);
+    });
+
+    test('should throw an error if there are no matches', async () => {
+      const matcher = new RegExp('no match');
+      const replacement = 'updated';
+
+      await expect(sut.update({ matcher, replacement })).rejects.toThrow(
+        'Error updating path/to/file.md. No matches found using matched: /no match/'
+      );
     });
   });
 });
--- libs/markdown/src/lib/Markdown/Markdown.ts
+++ libs/markdown/src/lib/Markdown/Markdown.ts
@@ -1,7 +1,27 @@
 import { Doc as Document } from '../Doc';
 
+type MarkdownUpdateParams = {
+  matcher: RegExp;
+  replacement: string;
+};
 export class Markdown extends Document {
   constructor(filePath: string) {
     super(filePath);
+  }
+
+  public async update({
+    matcher,
+    replacement,
+  }: MarkdownUpdateParams): Promise<void> {
+    const updatedValue = this.value.toString().replace(matcher, replacement);
+
+    if (updatedValue === this.value.toString()) {
+      throw new Error(
+        `Error updating ${this.path}. No matches found using matched: ${matcher}`
+      );
+    }
+
+    this.updatedValue = Buffer.from(updatedValue);
+    this.write(this.updatedValue);
   }
 }
