965c19018cb5241a96e6fa835cea794867a4f514
wijohnst <wijohnst@gmail.com>
2025-03-24 15:48:13 -0700

    chore: abstract class
    - Adds Chore abstract class

---
+++ libs/markdown/src/lib/Crawler/Crawler.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+
+export type CrawlerConfig = {
+  targetDir: string;
+};
+
+export abstract class Crawler<T extends CrawlerConfig> {
+  protected config: T;
+  protected fs: typeof fs;
+
+  public docs: string[] = [];
+
+  constructor(config: T, _fs: typeof fs = fs) {
+    this.config = config;
+    this.fs = _fs;
+
+    this.crawl();
+  }
+
+  abstract crawl(): void;
+}
